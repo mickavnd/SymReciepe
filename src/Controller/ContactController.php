6c5c17380814e8f30a21539b590679f3713374df
micka <mickavende.pro@gmail.com>
2023-01-16 22:46:15 +0100

    send mail on mailtrap

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,44 +9,61 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request ,EntityManagerInterface $manager, ): Response
+    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
+    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
     {
         $contact = new Contact();
 
-        if($this->getUser())
-                {
-                    // dd($this->getUser());
-            $contact->setFullName($this->getUser()->getFullName())
-                     ->setEmail($this->getUser()->getEmail());
-              }
+        // if($this->getUser())
+        //      {
+        //             // dd($this->getUser());
+        //        $contact->setFullName($this->getUser()->getFullName())
+        //              ->setEmail($this->getUser()->getEmail());
+        //       }
 
-        $form =$this->createForm(ContactType::class ,$contact);
+        $form = $this->createForm(ContactType::class, $contact);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $contact = $form->getData();
+            //email
+            $email = (new Email())
+                ->from('hello@example.com')
+                ->to('someone@example.com')
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject('Time for Symfony Mailer!')
+                ->text('Sending emails is fun again!')
+                ->html('<p>See Twig integration for better HTML integration!</p>');
 
+            $mailer->send($email);
             $manager->persist($contact);
 
             $manager->flush();
 
+
+
+
+
             $this->addFlash(
                 'success',
-                'le mot de passe a bien etait modifier'
+                'le mail a bien etait envoyer'
             );
 
             return $this->redirectToRoute('home.index');
-
         }
 
         return $this->render('pages/contact/index.html.twig', [
-             'form'=>$form->createView()
+            'form' => $form->createView()
         ]);
     }
 }
